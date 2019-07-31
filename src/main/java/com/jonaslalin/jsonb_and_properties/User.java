32db3b8ad398407fd0df69761a78343823f5f5cf
Jonas Lalin <1ac6df24@gmail.com>
2019-07-31 15:46:14 +0200

    initial commit

---
+++ src/main/java/com/jonaslalin/jsonb_and_properties/User.java
@@ -0,0 +1,31 @@
+package com.jonaslalin.jsonb_and_properties;
+
+public class User {
+    private String firstName;
+    private String middleInitial;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
